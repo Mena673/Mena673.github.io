fc31dea879f1dae3239a1188eb5446935f9b660c
Mena673 <157516687+Mena673@users.noreply.github.com>
2024-03-01 16:59:16 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -27,14 +27,14 @@ var init = function (window){
         function drawCircle(){
             // Code to draw a circle
             circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-            physikz.addRandomVelocity(circle, canvas, 2.5, 2.5);
+            physikz.addRandomVelocity(circle, canvas);
             view.addChild(circle);
             circles.push(circle);   
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
         var loopsCompleted = 10;
-        while (loopCompleted < 100) {
+        while (loopsCompleted < 100) {
             drawCircle() 
             loopsCompleted++;
         }
@@ -51,10 +51,10 @@ var init = function (window){
         function update() {
             // TODO 4 : Update the circle's position //
           
-          for (var i = 0; i < circles.length; i++){
+            for (var i = 0; i < circles.length; i++){
             physikz.updatePosition(circles[i]);
             game.checkCirclePosition(circles[i]);
-          }
+            }
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            // TODO 5 : Call game.checkCirclePosition() on your circles.
             // TODO 9 : Iterate over the array
@@ -84,7 +84,7 @@ var init = function (window){
                 circle.y = canvas.height;
             }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
-        
+        }
         
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
@@ -99,12 +99,13 @@ var init = function (window){
         game.update = update;
         
         app.addUpdateable(window.opspark.game);
-        }
-    };
+
+    }
+};
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
 // here, export any references you need for tests //
-module.exports = init;}
+module.exports = init;
     }
